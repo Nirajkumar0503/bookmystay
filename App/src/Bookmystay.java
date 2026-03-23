a4a760d642e13bfadabdbc07f299857d7ba1cc47
Nirajkumar0503 <niraj6201603681@gmail.com>
2026-03-23 15:01:23 +0530

    Niraj kumar initial commit

---
+++ App/src/Bookmystay.java
@@ -0,0 +1,6 @@
+public class Bookmystay {
+    static void main() {
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.);");
+    }
+}
